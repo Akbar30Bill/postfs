9752c48cc751ad30a893320943ae31bdc5de0d59
Ali Shahverdi <alishahverdi2010@yahoo.com>
2020-12-31 22:29:15 +0330

    fix pkey

--- postfs_init.py
+++ postfs_init.py
@@ -2,9 +2,10 @@ def postfs_init(cursor, config):
   cursor.execute('''
     create table entity(
       name text not null,
-      path text primary key,
+      path text not null,
       is_folder boolean default false not null,
-      binary_data bytea default null
+      binary_data bytea default null,
+      PRIMARY KEY (name, path, is_folder)
     )''')
   cursor.execute('''
     create table env(
